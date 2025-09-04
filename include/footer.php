71d9efeae6b13a705ddee1876e3e93270d175649
Gurdeep Singh <gurdeepsing1205@gmail.com>
2025-09-04 19:30:52 +0530

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,127 @@
+<!-- Widgets Section -->
+<div class="widgets-section">
+			<div class="auto-container">
+				<div class="row">
+					<!-- Footer COlumn -->
+					<div class="footer-column col-xl-3 col-lg-3 col-md-6 col-sm-12">
+						<div class="footer-widget about-widget">
+							<h5 class="widget-title">About</h5>
+							<div class="widget-content">
+								<div class="text">India’s top website designing, website development, 
+									digital marketing agency and SEO Services Company
+									Provides website designing & development services and Digital 
+									Marketing Services at affordable prices.</div>
+								<ul class="social-icon-two">
+									<li><a href="https://www.facebook.com/people/Digital-Making/61552338115206/"><i class="fab fa-facebook"></i></a></li>
+									<li><a href="https://www.instagram.com/digitalmaking7/"><i class="fab fa-instagram"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					
+					<!-- Footer COlumn -->
+					<div class="footer-column col-xl-2 col-lg-2 col-md-6 col-sm-12">
+						<div class="widget links-widget">
+							<h6 class="widget-title">Explore</h6>
+							<div class="widget-content">
+								<ul class="user-links">
+									<li><a href="services.php">Our Services</a></li>
+									<li><a href="clients.php">Our Clienst</a></li>
+									<li><a href="about.php">About Us</a></li>
+									<li><a href="contact.php">Contact</a></li>
+								</ul>                                
+							</div>
+						</div>
+					</div>
+
+
+					<!-- Footer COlumn -->
+					<div class="footer-column col-xl-3 col-lg-3 col-md-6 col-sm-12">
+						<div class="widget gallery-widget">
+							<h6 class="widget-title">Clients</h6>
+							<div class="widget-content">
+								<div class="outer">
+									<figure class="image">
+										<a href="clients.php"><img src="images/clients/1.jpg" alt=""></a>
+									</figure>
+									<figure class="image">
+										<a href="clients.php"><img src="images/clients/2.jpg" alt=""></a>
+									</figure>
+									<figure class="image">
+										<a href="clients.php"><img src="images/clients/3.jpg" alt=""></a>
+									</figure>
+									<figure class="image">
+										<a href="clients.php"><img src="images/clients/4.jpg" alt=""></a>
+									</figure>
+									<figure class="image">
+										<a href="clients.php"><img src="images/clients/5.jpg" alt=""></a>
+									</figure>
+									<figure class="image">
+										<a href="clients.php"><img src="images/clients/6.jpg" alt=""></a>
+									</figure>
+								</div>
+							</div>
+						</div>
+					</div>
+					
+					<!-- Footer COlumn -->
+					<div class="footer-column col-xl-4 col-lg-4 col-md-6 col-sm-12">
+						<div class="widget newsletter-widget">
+							<h6 class="widget-title">Connect us</h6>
+							<div class="widget-content">
+								<div class="inner">
+							
+							<div class="text">
+							<i style="color="white" class="fa fa-envelope" aria-hidden="true"></i>	<a href="mailto:digitalmaking97@gmail.com" class="text_gm">Digitalmaking97@gmail.com</a><br>
+							<i style="color="white" class="fa fa-phone" aria-hidden="true"></i>	<a href="tel:+91 9220364396" class="text_gm">+91 9220364396</a>
+							</div>
+						</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!--  Footer Bottom -->
+		<div class="footer-bottom">
+			<div class="auto-container">
+				<div class="row">
+					<div class="logo-column col-lg-4 col-md-4 order-2">
+						<div class="footer-logo"><a href="index.php"><img src="images/logo-2.png" alt=""></a></div>
+					</div>
+					<div class="links-column col-lg-4 col-md-4 order-3">
+						<ul class="footer-links">
+							<li><a href="#">Terms of Use</a></li>
+							<li><a href="#">Privacy Policy</a></li>
+						</ul>
+					</div>
+					<div class="col-lg-4 col-md-4">
+						<p class="copyright-text">© Copyright 2023 by DigitalMaking.com</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!--End Main Footer -->
+
+<!-- End Page Wrapper -->
+
+<!-- Scroll To Top -->
+<div class="scroll-to-top scroll-to-target" data-target="html"><span class="fa fa-angle-up"></span></div>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js" integrity="sha512-aVKKRRi/Q/YV+4mjoKBsE4x3H+BkegoM/em46NNlCqNTmUYADjBbeNefNxYV7giUp0VxICtqdrbqU7iVaeZNXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="js/popper.min.js"></script>
+<!-- Bootstrap JavaScript Libraries -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+  integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+  integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ include/otherheader.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Digital Making</title>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+<!-- Stylesheets -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+ integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+<link href="css/style.css" rel="stylesheet">
+
+<link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+<link rel="icon" href="images/icons/whatsApp.svg.wepg" type="image/x-icon">
+
+<!-- Responsive -->
+
+<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+
+<div class="page-wrapper">
+
+	
+
+	<!-- Main Header-->
+	<header class="main-header header-style-one">
+		<div class="header-top">
+			<div class="inner-container">
+		
+				<div class="top-left">
+					<ul class="list-style-one">
+						<li><i class="fa fa-envelope"></i> <a href="mailto:digitalmaking97@gmail.com">Digitalmaking97@gmail.com</a></li>
+						<li><i class="fa fa-map-marker"></i> Office address - 2nd floor, School Block, S - 698, Block C, Nirman Vihar, Shakarpur, Delhi, 110092</li>
+					</ul>
+				</div>
+		
+				<div class="top-right">
+					<ul class="social-icon-one">
+						<li><a href="https://www.facebook.com/people/Digital-Making/61552338115206/"><span class="fab fa-facebook-square"></span></a></li>
+						<li><a href="https://www.instagram.com/digitalmaking7/"><span class="fab fa-instagram"></span></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		
+		<div class="header-lower">
+			<!-- Main box -->
+			<div class="main-box">
+				<div class="logo-box">
+					<div class="logo"><a href="index.php"><img src="images/logo-2.png" alt="" title="Tronis"></a></div>
+
+					<!-- Header Search -->
+					<button class="ui-btn ui-btn search-btn">
+						<span class="icon lnr lnr-icon-search"></span>
+					</button>
+				</div>
+
+
+				<!--Nav Box-->
+				<div class="nav-outer">    
+					<nav class="nav main-menu">
+						<ul class="navigation">
+							<li class="dropdown"><a href="index.php">Home</a></li>
+							<li class="dropdown"><a href="about.php">About</a></li>
+							<li class="dropdown"><a href="services.php">Services</a></li>
+							<li class="dropdown"><a href="clients.php">Clients</a></li>
+							<li><a href="contact.php">Contact</a></li>
+						</ul>
+					</nav>
+					<!-- Main Menu End-->
+				</div>
+
+				<div class="outer-box">
+					<a href="tel:+91 9220364396" class="info-btn">
+						<i class="icon flaticon-phone-ringing"></i>
+						+91 9220364396
+					</a>
+					<!-- Mobile Nav toggler -->
+					<div class="mobile-nav-toggler"><span class="icon fa-solid fa-bars"></span></div>
+				</div>
+			</div>
+		</div>
+
+		<!-- Mobile Menu  -->
+		<div class="mobile-menu">
+			<div class="menu-backdrop"></div>
+		
+			<!--Here Menu Will Come Automatically Via Javascript / Same Menu as in Header-->
+			<nav class="menu-box">
+				<div class="upper-box">
+					<div class="nav-logo"><a href="index.php"><img src="images/logo.png" alt="" title=""></a></div>
+					<div class="close-btn"><i class="icon fa fa-times"></i></div>
+				</div>
+		
+				<ul class="navigation clearfix">
+					<!--Keep This Empty / Menu will come through Javascript-->
+				</ul>
+				<ul class="contact-list-one">
+					<li>
+						<!-- Contact Info Box -->
+						<div class="contact-info-box">
+							<i style="color="white" class="fa-fa-phone"></i>
+							<span class="title">Call Now</span>
+							<a href="tel:+91 9708766254/9801307801">+91 9708766254/9801307801</a>
+						</div>
+					</li>
+					<li>
+						<!-- Contact Info Box -->
+						<div class="contact-info-box">
+							<span class="icon lnr-icon-envelope1"></span>
+							<span class="title">Send Email</span>
+							<a href="mailto:digitalmaking97@gmail.com">Digitalmaking97@gmail.com</a>
+						</div>
+					</li>
+					<li>
+						<!-- Contact Info Box -->
+						<div class="contact-info-box">
+							<span class="icon lnr-icon-clock"></span>
+							<span class="title">Send Email</span>
+							Mon - Sat 10:00 am - 6:00 pm, Sunday - CLOSED
+						</div>
+					</li>
+				</ul>
+		
+		
+				<ul class="social-links">
+					<li><a href="https://www.facebook.com/people/Digital-Making/61552338115206/"><i class="fab fa-facebook-f"></i></a></li>
+					<li><a href="https://www.instagram.com/digitalmaking7/"><i class="fab fa-instagram"></i></a></li>
+				</ul>
+			</nav>
+		</div><!-- End Mobile Menu -->
+
+		
+
+		<!-- Sticky Header  -->
+		<div class="sticky-header">
+			<div class="auto-container">
+				<div class="inner-container">
+					<!--Logo-->
+					<div class="logo">
+						<a href="index.php" title=""><img src="images/logo-2.png" alt="" title=""></a>
+					</div>
+		
+					<!--Right Col-->
+					<div class="nav-outer">
+						<!-- Main Menu -->
+						<nav class="main-menu">
+							<div class="navbar-collapse show collapse clearfix">
+								<ul class="navigation clearfix">
+									<!--Keep This Empty / Menu will come through Javascript-->
+								</ul>
+							</div>
+						</nav><!-- Main Menu End-->
+		
+						<!--Mobile Navigation Toggler-->
+						<div class="mobile-nav-toggler"><span class="icon lnr-icon-bars"></span></div>
+					</div>
+				</div>
+			</div>
+		</div><!-- End Sticky Menu -->
+	</header>
+	<!--End Main Header -->
